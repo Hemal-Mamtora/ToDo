1e4a4482edb7c3b2b1a85758c01c1961c8f5ee5e
hemal <hemalmamtora1@gmail.com>
2018-10-07 14:54:28 +0530

    able to add data to database

--- app/src/main/java/com/hemalexample/todo/MainActivity.kt
+++ app/src/main/java/com/hemalexample/todo/MainActivity.kt
@@ -2,11 +2,43 @@ package com.hemalexample.todo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var etNewItem : EditText
+    lateinit var btnAddItem : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etNewItem = findViewById(R.id.etNewItem)
+        btnAddItem = findViewById(R.id.btnAddItem)
+
+        btnAddItem.setOnClickListener {
+            saveItem()
+        }
+    }
+
+    private fun saveItem(){
+        val item = etNewItem.text.toString().trim()
+        if(item.isEmpty()){
+            etNewItem.error = "Please enter a task"
+            return
+        }
+
+
+
+        val ref = FirebaseDatabase.getInstance().getReference("tasks")
+        val taskId = ref.push().key
+        val task = Task(taskId.toString(), item, "sample", false, Date(), 0)
+        ref.child(taskId.toString()).setValue(task).addOnCompleteListener {
+            Toast.makeText(applicationContext, "Task Saved Successfully", Toast.LENGTH_LONG).show()
+        }
     }
 }
---
+++ app/src/main/java/com/hemalexample/todo/Task.kt
@@ -0,0 +1,5 @@
+package com.hemalexample.todo
+
+import java.util.*
+
+class Task(val id: String, val task : String, val detail :String, val done : Boolean, val date: Date, val priority : Int)
